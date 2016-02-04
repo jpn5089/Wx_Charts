85cb69a74eeaaef739caf3e279efaabf9cde5f8d
jpn5089 <jp19nicola@gmail.com>
2016-02-03 19:50:19 -0500

    Included all months now

--- Hourly Temp Data.R
+++ Hourly Temp Data.R
@@ -18,58 +18,61 @@ StationsRow <- c(6,14,1,13,7,16,5)
 allData <- list()
 
 for (i in 1:7){
-
-jan_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]), 
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-01-01', enddate = '2010-02-01', limit = 1000)
-
-feb_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]), 
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-02-01', enddate = '2010-03-01', limit = 1000)
-
-mar_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-03-01', enddate = '2010-04-01', limit = 1000)
-
-apr_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-04-01', enddate = '2010-05-01', limit = 1000)
-
-may_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-05-01', enddate = '2010-06-01', limit = 1000)
-
-jun_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-06-01', enddate = '2010-07-01', limit = 1000)
-
-jul_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-07-01', enddate = '2010-08-01', limit = 1000)
-
-aug_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-08-01', enddate = '2010-09-01', limit = 1000)
-
-sep_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-09-01', enddate = '2010-10-01', limit = 1000)
-
-oct_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-10-01', enddate = '2010-11-01', limit = 1000)
-
-nov_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
-     datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-11-01', enddate = '2010-12-01', limit = 1000)
-
-normals <- rbind(jan_normals[[2]] %>% filter(month(date) != 2),feb_normals[[2]] %>% filter(month(date) != 3),
-                 mar_normals[[2]] %>% filter(month(date) != 4),apr_normals[[2]] %>% filter(month(date) != 5), 
-                 may_normals[[2]] %>% filter(month(date) != 6),jun_normals[[2]] %>% filter(month(date) != 7),
-                 jul_normals[[2]] %>% filter(month(date) != 8),aug_normals[[2]] %>% filter(month(date) != 9), 
-                 sep_normals[[2]] %>% filter(month(date) != 10),oct_normals[[2]] %>% filter(month(date) != 11),
-                 nov_normals[[2]] %>% filter(month(date) != 12)) %>%
-  mutate(date = ymd_hms(gsub("T"," ",date)) + years(6),
-         value = (value/10) ) %>%
-  select(date,station,value) %>%
-  mutate(value = as.numeric(value), datatype = "Normal")%>%
-  mutate(day = floor_date(date,unit = "day"),
-         hour = hour(date)) %>%
-  mutate(station = as.character(Stations[StationsRow[i],1]))
-
-allData[[i]] <- normals
+  
+  jan_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]), 
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-01-01', enddate = '2010-02-01', limit = 1000)
+  
+  feb_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]), 
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-02-01', enddate = '2010-03-01', limit = 1000)
+  
+  mar_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-03-01', enddate = '2010-04-01', limit = 1000)
+  
+  apr_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-04-01', enddate = '2010-05-01', limit = 1000)
+  
+  may_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-05-01', enddate = '2010-06-01', limit = 1000)
+  
+  jun_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-06-01', enddate = '2010-07-01', limit = 1000)
+  
+  jul_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-07-01', enddate = '2010-08-01', limit = 1000)
+  
+  aug_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-08-01', enddate = '2010-09-01', limit = 1000)
+  
+  sep_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-09-01', enddate = '2010-10-01', limit = 1000)
+  
+  oct_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-10-01', enddate = '2010-11-01', limit = 1000)
+  
+  nov_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-11-01', enddate = '2010-12-01', limit = 1000)
+  
+  dec_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]),
+                      datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-12-01', enddate = '2011-01-01', limit = 1000)
+  
+  normals <- rbind(jan_normals[[2]] %>% filter(month(date) != 2),feb_normals[[2]] %>% filter(month(date) != 3),
+                   mar_normals[[2]] %>% filter(month(date) != 4),apr_normals[[2]] %>% filter(month(date) != 5), 
+                   may_normals[[2]] %>% filter(month(date) != 6),jun_normals[[2]] %>% filter(month(date) != 7),
+                   jul_normals[[2]] %>% filter(month(date) != 8),aug_normals[[2]] %>% filter(month(date) != 9), 
+                   sep_normals[[2]] %>% filter(month(date) != 10),oct_normals[[2]] %>% filter(month(date) != 11),
+                   nov_normals[[2]] %>% filter(month(date) != 12),dec_normals[[2]] %>% filter(month(date) != 1)) %>%
+    mutate(date = ymd_hms(gsub("T"," ",date)) + years(6),
+           value = (value/10) ) %>%
+    select(date,station,value) %>%
+    mutate(value = as.numeric(value), datatype = "Normal")%>%
+    mutate(day = floor_date(date,unit = "day"),
+           hour = hour(date)) %>%
+    mutate(station = as.character(Stations[StationsRow[i],1]))
+  
+  allData[[i]] <- normals
 }
 
 normalAll <-do.call(rbind,allData)
 
-write.csv(normalAll, file = "C:/Users/jnicola/Desktop/Normals2016.csv")
-  
+write.csv(normalAll, file = "C:/Users/jnicola/Desktop/Weather Charts/Temp_Normals.csv")
+
