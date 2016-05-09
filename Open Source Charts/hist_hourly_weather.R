1343148216432b9e57f2e5993eba8c42200edc98
jpn5089 <jp19nicola@gmail.com>
2016-05-09 14:18:56 -0400

    Updates

--- Open Source Charts/hist_hourly_weather.R
+++ Open Source Charts/hist_hourly_weather.R
@@ -13,17 +13,17 @@ pa.list <- st %>%
 
 # download the historic file
 path <- sprintf("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/%s/%s-%s-2016.gz",2016,pa.list$USAF[1],pa.list$WBAN[1])
-download.file(path, destfile = "/Users/John/Desktop/data.gz")
+download.file(path, destfile = "C:/Users/John/Documents/GitHub/Wx_Charts/Open Source Charts/data.gz")
 
 # unzip
 library(R.utils)
-gunzip("/Users/John/Desktop/data.gz")
+gunzip("C:/Users/John/Documents/GitHub/Wx_Charts/Open Source Charts/data.gz")
 #will save a text file under the same name as the zip file
 column.widths <- c(4, 6, 5, 4, 2, 2, 2, 2, 1, 6,
                    7, 5, 5, 5, 4, 3, 1, 1, 4, 1, 5, 1, 1, 1, 6,
                    1, 1, 1, 5, 1, 5, 1, 5, 1)
 
-raw_data <- read.fwf("/Users/John/Desktop/data", column.widths)
+raw_data <- read.fwf("C:/Users/John/Documents/GitHub/Wx_Charts/Open Source Charts/data", column.widths)
 
 selected_data <- raw_data[, c(2:8, 10:11, 13, 16, 19, 29,
                   31, 33)]
@@ -39,9 +39,16 @@ names(selected_data) <- c("USAFID", "WBAN", "YR", "M",
 # WIND.SPD in mph
 final_data <- selected_data %>%
   mutate_each(funs(replace(., . %in% c(999,9999,99999), NA))) %>%
-  mutate (TEMP = ((TEMP/10)*(9/5) +32),
-          DEW.POINT = ((DEW.POINT/10)*(9/5) +32),
+  mutate (TEMP = round(((TEMP/10)*(9/5) +32),0),
+          DEW.POINT = round(((DEW.POINT/10)*(9/5) +32)),
           ATM.PRES = ATM.PRES/10,
           WIND.SPD = round(((WIND.SPD/10)*2.23694), 0))
 
+out <- list()
+
+out <- final_data
+
+final_data <- do.call(rbind,out) %>%
+  group_by(YR, M, D, HR) %>%
+  summarize_each(funs(mean(., na.rm = TRUE)))
 
