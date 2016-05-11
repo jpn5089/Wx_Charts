d57296b891595934fbd73ccc80086575a985e227
cam333 <cmohaniii@gmail.com>
2016-05-11 13:29:35 -0400

    update

--- Data/Hourly Temp Data.R
+++ Data/Hourly Temp Data.R
@@ -3,7 +3,7 @@ library(lubridate)
 library(rnoaa)
 library(ggplot2)
 
-options(noaakey = "insert key here")
+options(noaakey = "gOTjkNrzDlVAafWzJyICPLKaWmwXpMoL")
 
 Stations <- read.csv("c:/users/John/Documents/GitHub/Wx_Charts/Data/StationNames.csv",stringsAsFactors = FALSE)
 
@@ -13,11 +13,11 @@ Stations <- read.csv("c:/users/John/Documents/GitHub/Wx_Charts/Data/StationNames
 #hours <- seq(0,23)
 #temps <- c("ATSI_temp","DUQ_temp","PPL_temp","PECO_temp","AEP_temp","Toledo_temp","Akron_temp")
 
-StationsRow <- c(6,14,1,13,7,16,5)
+StationsRow <- c(6,20)
 
 allData <- list()
 
-for (i in 1:7){
+for (i in 1:2){
   
   jan_normals <- ncdc(datasetid='NORMAL_HLY', stationid = as.character(Stations[StationsRow[i],2]), 
                       datatypeid = "HLY-TEMP-NORMAL", startdate = '2010-01-01', enddate = '2010-02-01', limit = 1000)
@@ -74,5 +74,7 @@ for (i in 1:7){
 
 normalAll <-do.call(rbind,allData)
 
-write.csv(normalAll, file = "C:/Users/jnicola/Desktop/Weather Charts/Temp_Normals.csv")
+write.csv(normalAll, file = "C:\\Users\\John\\Desktop\\Temp_Norm.csv")
+
+#write.csv(normalAll, file = "C:\Users\John\Documents\GitHub\Wx_Charts\Data\Temp_Normals.csv")
 
--- Open Source Charts/hist_hourly_weather.R
+++ Open Source Charts/hist_hourly_weather.R
@@ -1,9 +1,9 @@
-
+library(R.utils)
 # Get all the possible stations
 st <- read.csv(file = "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.csv") %>%
   mutate( LAT = LAT / 1000,
           LON = LON / 1000,
-          ELEV.M. = ELEV.M. / 10,
+          ELEV.M. = ELEV.M.,
           BEGIN = as.numeric(substr(BEGIN, 1, 4)),
           END = as.numeric(substr(END, 1, 4)))
 
@@ -12,18 +12,32 @@ pa.list <- st %>%
     filter(grepl("PITTSBURGH",STATION.NAME))
 
 # download the historic file
+<<<<<<< HEAD
+out <- list()
+for (i in c(2013,2014,2015,2016)){
+path <- sprintf("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/%s/%s-%s-%s.gz",i,pa.list$USAF[1],pa.list$WBAN[1],i)
+download.file(path, destfile = sprintf("/Users/cmohan/Desktop/data_%s.gz", i))
+
+# unzip
+gunzip(sprintf("/Users/cmohan/Desktop/data_%s.gz", i))
+=======
 path <- sprintf("ftp://ftp.ncdc.noaa.gov/pub/data/noaa/%s/%s-%s-2016.gz",2016,pa.list$USAF[1],pa.list$WBAN[1])
-download.file(path, destfile = "/Users/cmohan/Desktop/data.gz")
+download.file(path, destfile = "C://Users/John/Desktop/data.gz")
 
 # unzip
 library(R.utils)
-gunzip("/Users/cmohan/Desktop/data.gz")
+gunzip("C:/Users/John/Desktop/data.gz")
+>>>>>>> origin/master
 #will save a text file under the same name as the zip file
 column.widths <- c(4, 6, 5, 4, 2, 2, 2, 2, 1, 6,
                    7, 5, 5, 5, 4, 3, 1, 1, 4, 1, 5, 1, 1, 1, 6,
                    1, 1, 1, 5, 1, 5, 1, 5, 1)
 
-raw_data <- read.fwf("/Users/cmohan/Desktop/data", column.widths)
+<<<<<<< HEAD
+raw_data <- read.fwf(sprintf("/Users/cmohan/Desktop/data_%s", i), column.widths)
+=======
+raw_data <- read.fwf("C:/Users/John/Desktop/data", column.widths)
+>>>>>>> origin/master
 
 selected_data <- raw_data[, c(2:8, 10:11, 13, 16, 19, 29,
                   31, 33)]
@@ -33,7 +47,36 @@ names(selected_data) <- c("USAFID", "WBAN", "YR", "M",
                  "WIND.DIR", "WIND.SPD", "TEMP", "DEW.POINT",
                  "ATM.PRES")
 
-final_data <- selected_data %>%
+<<<<<<< HEAD
+x <- selected_data %>%
   mutate_each(funs(replace(., . %in% c(999,9999,99999), NA)))
 
+name <- paste(i)
+out[[name]] <- x
+}
+=======
+# In UTC time...so need to subtract 4 hours to get to EST local
+# TEMP and DEW.POINT in degrees F
+# ATM.PRES in mb
+# WIND.SPD in mph
+final_data <- selected_data %>%
+  mutate_each(funs(replace(., . %in% c(999,9999,99999), NA))) %>%
+  mutate (TEMP = round(((TEMP/10)*(9/5) +32),0),
+          DEW.POINT = round(((DEW.POINT/10)*(9/5) +32)),
+          ATM.PRES = ATM.PRES/10,
+          WIND.SPD = round(((WIND.SPD/10)*2.23694), 0))
 
+out <- list()
+
+out <- final_data
+
+final_data <- do.call(rbind,out) %>%
+  group_by(YR, M, D, HR) %>%
+  summarize_each(funs(mean(., na.rm = TRUE)))
+>>>>>>> origin/master
+
+final_data <- do.call(rbind,out) %>%
+  group_by(YR, M, D, HR) %>%
+  summarize_each(funs(mean(., na.rm = TRUE)))
+  
+write.csv(final_data, file = "/Users/cmohan/Desktop/duq_weather.csv")
