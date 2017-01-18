544d0b0d3c0f2d9d9bc16faa6e265132ecec93a8
jpn5089 <jp19nicola@gmail.com>
2017-01-18 14:42:30 -0500

    Updates

--- Open Source Charts/DarkSky/DarkSky.R
+++ Open Source Charts/DarkSky/DarkSky.R
@@ -18,8 +18,6 @@ packageVersion("darksky")
 
 Sys.getenv("DARKSKY_API_KEY")
 
-darksky_api_key = "a6eb6ed5246892a0d0988713d3b17102"
-
 station_info <- read.csv(file = "C:\\Users\\John\\Documents\\GitHub\\Wx_Charts\\Data\\isd-history.csv")
 
 now <- get_current_forecast(43.2672, -70.8617)
